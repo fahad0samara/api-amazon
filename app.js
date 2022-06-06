63847b95baf3c76490f2eccaefe6ae3678b069e4
fahad0samara <fah8ad@outlook.com>
2022-06-06 15:50:14 +0300

    make it better

--- app.js
+++ app.js
@@ -1,51 +1,56 @@
 const express = require('express');
 const app = express();
+const request = require('request-promise');
 const path = require('path');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const apikey = 'e4037415b3d58b93e689d4ed83405ffb'
+const baseurl = `http://api.scraperapi.com?api_key=${apikey}&autoparse=true`
 
 app.set('port', process.env.port || 3000)
-
+app.use(express.json())
 app.get('/', (req, res, next) => {
     res.send('<h1>Hello world<h1>');
 })
 
-app.get('/news', (req, res, next) => {
-    axios('https://www.amazon.com').then((response) => {
-            const html = response.data;
-            const $ = cheerio.load(html);
-            const data = []
-            $('.a-section grid-row-1', html).each(function() {
-                data.push($(this).text())
-            })
+app.get('/products/:productId', async(req, res, next) => {
+    const { productId } = req.params;
+    try {
+        const response = await request(`${baseurl}&url=https://www.amazon.com/dp/${productId}`);
+        res.json(JSON.parse(response));
 
-            dataBuffer = {
-                text: $('a-section grid-row-1').text(),
-                textContent: data
-            }
-            res.json(dataBuffer)
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-
+    } catch (error) {
+        res.json(error)
+    }
 })
-app.get('/data', (req, res, next) => {
-    axios('https://rapidapi.com/apidojo/api/asos2/').then((response) => {
-        const html = response.data;
-        const $ = cheerio.load(html);
-        const data = $('.ProductInfo ')
-        const dataBuffer = data.find('div').text()
-        console.log(data.text(), 'ffff');
-        console.log(dataBuffer, 'bbbb');
-        res.json(dataBuffer)
 
-    }).catch((error) => {
-        console.log(error)
-    })
+app.get('/products/:productId/reviews', async(req, res, next) => {
+    const { productId } = req.params;
+    try {
+        const response = await request(`${baseurl}&url=https://www.amazon.com/product-reviews/${productId}`);
+        res.json(JSON.parse(response));
 
+    } catch (error) {
+        res.json(error)
+    }
 })
+app.get('/search/:searchid', async(req, res, next) => {
+    const { searchid } = req.params;
+    try {
+        const response = await request(`${baseurl}&url=https://www.amazon.com/s?k=/${searchid}`);
+        res.json(JSON.parse(response));
+
+    } catch (error) {
+        res.json(error)
+    }
+})
+
+
+
+
 
 app.listen(app.get('port'), server => {
-    console.info(`Server listen on port ${app.get('port')}`);
+    console.info(`
+Server listen on port $ { app.get('port') }
+`);
 })
